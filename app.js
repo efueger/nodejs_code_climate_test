f31f4d1579186791a83c51d603ce4c75a3d6b966
Eamonn McEvoy <emcevoy@Eamonns-MacBook-Pro.local>
2019-04-03 22:48:45 +0200

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+var a = 1;
+let b = 1;
+const c = 1;
+
+function unusedFunc() {
+    if(true)
+        return false;
+    return 1;
+}
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
